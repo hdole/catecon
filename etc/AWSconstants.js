1e2450ae811b052c0d71df61ea87d08f9b230401
Harry Dole <harry@harrydole.com>
2018-09-21 23:09:26 -0700

    Add constants and build script.

---
+++ etc/AWSconstants.js
@@ -0,0 +1,27 @@
+if(typeof exports == "undefined")
+{
+	exports = this;
+}
+
+const AWSConstants = function()
+{
+	this.init();
+};
+
+AWSConstants.prototype =
+{
+	init: function()
+	{
+		exports.CATECON_TOPIC = 'arn:aws:sns:us-west-1:395668725886:Catecon';
+		exports.COGNITO_REGION = 'us-west-2';
+		exports.DIAGRAM_BUCKET_NAME = 'catecon-diagrams';
+		exports.DIAGRAM_TABLE = 'Catecon-users';
+		exports.IDENTITY_POOL_ID = 'us-west-2:d7948fb7-c661-4d0f-8702-bd3d0a3e40bf';
+		exports.NEW_USER_QUEUE = 'https://sqs.us-west-2.amazonaws.com/395668725886/CateconNewUserQueue';
+		exports.NEW_USER_TOPIC = 'arn:aws:sns:us-west-2:395668725886:CateconNewUser';
+		exports.RECENT_DIAGRAM_TABLE = 'Catecon-recent';
+		exports.REGION = 'us-west-1';
+	}
+};
+
+exports.AWSConstants = new AWSConstants();
