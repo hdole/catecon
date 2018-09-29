3fb3027ada923aa25287a2d2eafea59df2bc30f7
Harry Dole <harry@harrydole.com>
2018-09-29 10:22:12 -0700

    Improved

--- etc/build.js
+++ etc/build.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // (C) 2018 Harry Dole
 // Catecon:  The Categorical Console
 //
@@ -7,25 +8,66 @@ const proc = require('child_process');
 
 AWS.config.loadFromPath('/home/hdole/.aws/config.json');
 
-let lambda = new AWS.Lambda();
-// let lambda = new AWS.Lambda({region:'us-west-2'});
-
-let lambdas = ['CateconBootstrap',
+let lambdas = [
+				'CateconBootstrap',
 				'CateconCatalog',
 				'CateconDiagramUpdate',
 				'CateconDownloadJS',
 				'CateconGetUserDiagrams',
 				'CateconIngestCategory',
 				'CateconIngestDiagram',
-				'CateconNewUser',
+//				'CateconNewUser',
 				'CateconRecentDiagrams',
-				'CateconUserScanner',
+//				'CateconUserScanner',
 			];
+
+const root = '/mnt/f/xampp/htdocs/catecon2/Catecon-web';
+const nodeModules = '/mnt/f/xampp/htdocs/node_modules';
+const lambdaRoot = `${root}/lambda`;
+
+const dependencies =
+{
+	CateconBootstrap:	['Cat.js', 'CatFns.js', 'sha256.js', 'sjcl.js', 'peg-0.10.0.min.js', 'amazon-cognito-identity.min.js', 'AWSconstants.js'],
+	CateconCatalog:		['AWSconstants.js'],
+	CateconGetUserDiagrams:		['AWSconstants.js'],
+	CateconIngestDiagram:		['AWSconstants.js'],
+	CateconRecentDiagrams:		['AWSconstants.js'],
+};
+
+const locations =
+{
+	'AWSconstants.js':	`${root}/lambda`,
+	'Cat.js':			`${root}`,
+	'CatFns.js':		`${root}`,
+	'sha256.js':		`${root}`,
+	'sjcl.js':			`${root}`,
+	'peg-0.10.0.min.js':	`${root}`,
+	'amazon-cognito-identity.min.js':	`${nodeModules}/amazon-cognito-identity-js/dist`,
+};
+
+const buildDir = 'build';
+
+if (!fs.existsSync(buildDir))
+	fs.mkdirSync(buildDir);
+process.chdir(buildDir);
 
 lambdas.map(f =>
 {
-	proc.exec(`cp ${f}.js index.js`);
-	proc.exec(`zip ${f}.zip index.js AWSconstants.js`);
-	proc.exec('rm index.js');
-	proc.exec(`aws lambda update-function-code --function-name ${f} --zip-file fileb://./${f}.zip`);
+	console.log(`Diagram ${f} uploading...`);
+	let deps = [];
+	let copyDeps = '';
+	if (f in dependencies)
+	{
+		deps = dependencies[f];
+		copyDeps = deps.map(d => `cp -p ${locations[d]}/${d} .`).join(';\n');
+	}
+	const script =
+`${copyDeps}
+cp ${lambdaRoot}/${f}.js index.js
+zip ${f}.zip index.js ${deps.join(' ')}
+rm index.js
+aws lambda update-function-code --function-name ${f} --zip-file fileb://./${f}.zip
+`;
+
+	proc.execSync(script); //.on('exit', function(code, signal) {console.log(`${f} uploaded with status ${code}`)});
 });
