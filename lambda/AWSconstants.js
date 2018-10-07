839309cb9b918947ccdf9f4721007d1b73b97ae4
Harry Dole <harry@harrydole.com>
2018-10-07 11:09:57 -0700

    Add CT contact table

--- lambda/AWSconstants.js
+++ lambda/AWSconstants.js
@@ -15,15 +15,16 @@ AWSConstants.prototype =
 {
 	init: function()
 	{
-		exports.CATECON_TOPIC = 'arn:aws:sns:us-west-1:395668725886:Catecon';
-		exports.COGNITO_REGION = 'us-west-2';
-		exports.DIAGRAM_BUCKET_NAME = 'catecon-diagrams';
-		exports.DIAGRAM_TABLE = 'Catecon-users';
-		exports.IDENTITY_POOL_ID = 'us-west-2:d7948fb7-c661-4d0f-8702-bd3d0a3e40bf';
-		exports.NEW_USER_QUEUE = 'https://sqs.us-west-2.amazonaws.com/395668725886/CateconNewUserQueue';
-		exports.NEW_USER_TOPIC = 'arn:aws:sns:us-west-2:395668725886:CateconNewUser';
-		exports.RECENT_DIAGRAM_TABLE = 'Catecon-recent';
-		exports.REGION = 'us-west-1';
+		exports.CATECON_TOPIC =			'arn:aws:sns:us-west-1:395668725886:Catecon';
+		exports.COGNITO_REGION =		'us-west-2';
+		exports.DIAGRAM_BUCKET_NAME =	'catecon-diagrams';
+		exports.DIAGRAM_TABLE =			'Catecon-users';
+		exports.CT_TABLE =				'Catecon-ct';
+		exports.IDENTITY_POOL_ID =		'us-west-2:d7948fb7-c661-4d0f-8702-bd3d0a3e40bf';
+		exports.NEW_USER_QUEUE =		'https://sqs.us-west-2.amazonaws.com/395668725886/CateconNewUserQueue';
+		exports.NEW_USER_TOPIC =		'arn:aws:sns:us-west-2:395668725886:CateconNewUser';
+		exports.RECENT_DIAGRAM_TABLE =	'Catecon-recent';
+		exports.REGION =				'us-west-1';
 	}
 };
 
