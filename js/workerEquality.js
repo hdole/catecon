84c21eebc3622108ce60c6091f28bf741d1b4b63
Harry Dole <harry@harrydole.com>
2020-04-05 16:30:30 -0700

    no longer return items

--- js/workerEquality.js
+++ js/workerEquality.js
@@ -1,7 +1,6 @@
 // (C) 2020 Harry Dole
 // Catecon equality worker
 
-const equivalences = new Set;
 const sig2equivalences = new Map;		// sig to the list of pairs [leg, item]
 const equals = new Map;
 const items = new Map;
@@ -43,7 +42,6 @@ onmessage = function(e)
 					items:			items.size,
 					maxLegLength,
 					equals:			equals.size,
-					equivalences:	equivalences.size,
 					sigs:			sig2equivalences.size,
 				};
 				break;
@@ -58,7 +56,7 @@ onmessage = function(e)
 	}
 }
 
-const Sig = function(...elts)
+function Sig(...elts)
 {
 	if (elts.length === 0)
 		throw 'no info';
@@ -67,33 +65,95 @@ const Sig = function(...elts)
 	return sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(elts.join()));
 }
 
-const LoadEquivalence = function(item, leftLeg, rightLeg)
+function LoadSigLeg(sig, leg)
+{
+	if (!sig2equivalences.has(sig))
+		sig2equivalences.set(sig, []);
+	const equs = sig2equivalences.get(sig);
+	equs.push(leg);
+}
+
+function LoadEquivalence(item, leftLeg, rightLeg)
 {
 	maxLegLength = Math.max(maxLegLength, leftLeg.length, rightLeg.length)
-	if (!items.has(item))
-		items.set(item, new Map);
-	const itemEqus = items.get(item);
 	const leftSig = Sig(...leftLeg);
 	const rightSig = Sig(...rightLeg);
-	itemEqus.set(Sig(leftSig, rightSig), [leftLeg, rightLeg]);
+	if (item)
+	{
+		if (!items.has(item))
+			items.set(item, new Map);
+		const itemEqus = items.get(item);
+		itemEqus.set(Sig(leftSig, rightSig), [leftLeg, rightLeg]);
+	}
 	const leftSigs = equals.has(leftSig) ? equals.get(leftSig) : new Set;
 	const rightSigs = equals.has(rightSig) ? equals.get(rightSig) : new Set;
 	leftSigs.add(rightSig);
 	rightSigs.forEach(function(s) { leftSigs.add(s); });
 	equals.set(leftSig, leftSigs);
 	equals.set(rightSig, leftSigs);	// since equal set is same
-	const fn = function(sig, leg, item)
-	{
-		if (!sig2equivalences.has(sig))
-			sig2equivalences.set(sig, []);
-		const equs = sig2equivalences.get(sig);
-		equs.push([leg, item]);
-	}
-	fn(leftSig, rightLeg, item);
+	LoadSigLeg(leftSig, rightLeg);
 	if (leftSig !== rightSig)
-		fn(rightSig, leftLeg, item);
-	equivalences.add([item, leftLeg, rightLeg]);
-	return null;
+		LoadSigLeg(rightSig, leftLeg);
+	return true;
+}
+
+function CompareSigs(leftSig, rightSig)
+{
+	const equ = equals.get(leftSig);
+	return equ ? equ.has(rightSig) : false;
+}
+
+function TryAlternateLeg(leg, ndx, cnt, sig, subLeg, altLeg)
+{
+	let isEqual = false;
+	if (altLeg.length < subLeg.length)
+	{
+		const nuLeg = leg.slice(0, ndx);
+		nuLeg.push(...altLeg);
+		if (ndx + cnt < leg.length)
+			nuLeg.push(...leg.slice(ndx + cnt, leg.length));
+		const nuSig = Sig(...nuLeg);
+		if (sig === nuSig)
+			isEqual = true;
+		else
+		{
+			const sigEqus = equals.get(sig);
+			const nuSigEqus = equals.get(nuSig);
+			if ((sigEqus && sigEqus.has(nuSig)) || (nuSigEqus && nuSigEqus.has(sig)))
+				isEqual = true;
+		}
+		if (!isEqual)
+			isEqual = ScanLeg(nuLeg, sig);
+		if (isEqual)
+			LoadEquivalence(null, leg, nuLeg);
+	}
+	return isEqual;
+}
+
+function CheckLeg(leg, ndx, cnt, sig)
+{
+	let isEqual = false;
+	const subLeg = leg.slice(ndx, ndx + cnt);
+	const subSig = Sig(...subLeg);
+	if (sig2equivalences.has(subSig))
+	{
+		const subSet = sig2equivalences.get(subSig);
+		for (const altLeg of subSet)
+			if (isEqual = TryAlternateLeg(leg, ndx, cnt, sig, subLeg, altLeg))
+				break;
+	}
+	return isEqual;
+}
+
+function ScanLeg(leg, sig)
+{
+	const len = leg.length;
+	if (len > 2)
+		for (let ndx=0; ndx < len-1; ++ndx)
+			for (let cnt=2; cnt <= Math.min(maxLegLength, len - ndx); ++cnt)
+				if (CheckLeg(leg, ndx, cnt, sig))
+					return true;
+	return false;
 }
 
 function CheckEquivalence(diagram, cell, leftLeg, rightLeg)
@@ -102,88 +162,15 @@ function CheckEquivalence(diagram, cell, leftLeg, rightLeg)
 		Reload();
 	const leftSig = Sig(...leftLeg);
 	const rightSig = Sig(...rightLeg);
-	let item = null;
 	let isEqual = false;
-	const fn = function(leg, leftSig, rightSig)
+	isEqual = CompareSigs(leftSig, rightSig);
+	if (!isEqual)
 	{
-		const equ = equals.get(leftSig);
-		if (equ && equ.has(rightSig))
-		{
-			isEqual = true;
-			const equs = sig2equivalences.get(rightSig);	// item search
-			if (equs)
-				for (let i=0; i< equs.length; ++i)
-				{
-					const equ = equs[i];
-					const sig = Sig(...equ[0]);
-					if (sig === leftSig)
-					{
-						item = equ[1];
-						break;
-					}
-				}
-		}
-		return item;
-	};
-	item = fn(leftLeg, leftSig, rightSig);
-	if (!item)
-	{
-		const fn = function(leg, sig)
-		{
-			const len = leg.length;
-			if (len > 2)
-				for (let ndx=0; ndx < len-1; ++ndx)
-					for (let cnt=2; cnt <= Math.min(maxLegLength, len - ndx); ++cnt)
-					{
-						item = CheckLeg(leg, ndx, cnt, sig);
-						if (item)
-						{
-							isEqual = true;
-							return item;
-						}
-					}
-			return null;
-		};
-		item = fn(leftLeg, rightSig);
-		if (!item)
-			item = fn(rightLeg, leftSig);
+		isEqual = ScanLeg(leftLeg, rightSig);
+		if (!isEqual)
+			isEqual = ScanLeg(rightLeg, leftSig);
 	}
-	return {diagram, cell, isEqual, item};
-}
-
-function CheckLeg(leg, ndx, cnt, sig)
-{
-	let item = null;
-	const subLeg = leg.slice(ndx, ndx + cnt);
-	const subSig = Sig(...subLeg);
-	if (sig2equivalences.has(subSig))
-	{
-		const subSet = sig2equivalences.get(subSig);
-		subSet.forEach(function(s)
-		{
-			const altLeg = s[0];
-			if (altLeg.length < subLeg.length)
-			{
-				const nuLeg = leg.slice(0, ndx);
-				nuLeg.push(...altLeg);
-				if (ndx + cnt < leg.length)
-					nuLeg.push(...leg.slice(ndx + cnt, leg.length));
-				const nuSig = Sig(...nuLeg);
-				if (sig === nuSig)
-					item = s[1];
-				else
-				{
-					const sigEqus = equals.get(sig);
-					const nuSigEqus = equals.get(nuSig);
-					if ((sigEqus && sigEqus.has(nuSig)) || (nuSigEqus && nuSigEqus.has(sig)))
-						item = s[1];
-				}
-				if (item)
-					LoadEquivalence(item, leg, nuLeg);
-			}
-		});
-	}
-	return item;
+	return {diagram, cell, isEqual};
 }
 
 function RemoveEquivalences(delItems)
@@ -195,7 +182,6 @@ function RemoveEquivalences(delItems)
 function Reload()
 {
 	maxLegLength = 0;
-	equivalences.clear();
 	sig2equivalences.clear();
 	equals.clear();
 	items.forEach(function(equs, item)
