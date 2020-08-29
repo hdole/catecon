4ffdc5cdc220a4a6cefa24750eccf674172c192b
Harry Dole <harry@harrydole.com>
2020-08-28 22:55:36 -0700

    ingests compressed and emits compressed to s3

--- lambda/CateconIngestDiagram.js
+++ lambda/CateconIngestDiagram.js
@@ -3,6 +3,7 @@
 //
 const AWS = require('aws-sdk');
 const C = require('./AWSconstants.js');
+const zlib = require('zlib');
 
 const credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId:C.IDENTITY_POOL_ID});
 
@@ -16,114 +17,93 @@ const S3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 exports.handler = (event, context, callback) =>
 {
-	const diagram = event.diagram;
-	const user = 'user' in event ? event.user : 'hfdole';
-	if (diagram.user !== user)
+	const bodyBuffer = Buffer.from(event.body, 'base64');
+	zlib.gunzip(bodyBuffer, (error, bodyStr) =>
 	{
-		const message = `Error:  User ${user} is not the owner of ${diagram.name}.  ${diagram.user} is.`;
-		console.log(message);
-		callback(message, null);
-		return;
-	}
-	const Body = JSON.stringify(diagram);
-	if (Body.length > 1000000)
-	{
-		const message = 'Error:  Diagram is too large to load';
-		console.log(message);
-		callback(message, null);
-		return;
-	}
-	const URL = `https://s3-${C.REGION}.amazonaws.com/${C.DIAGRAM_BUCKET_NAME}`;
-	const bucket = new AWS.S3({apiVersion:'2006-03-01', params: {Bucket: C.DIAGRAM_BUCKET_NAME}});
-	const name = `${user}/${diagram.basename}`;
-	const Key = `${name}.json`;
-	const s3params =
-	{
-		Bucket:  		C.DIAGRAM_BUCKET_NAME,
-		ContentType:	'json',
-		Key,
-		Body,
-		ACL:	 		'public-read',
-	};
-	bucket.putObject(s3params, function(err, data)
-	{
-		if (err)
+		if (error)
+			return callback(error);
+		const body = JSON.parse(bodyStr);
+		const timestamp = Date.now();
+		const user = body.user;
+		const diagram = body.diagram;
+		const png = body.png;
+		diagram.timestamp = timestamp;
+		const diagramString = JSON.stringify(diagram);
+		zlib.gzip(diagramString, (error, buffer) =>
 		{
-			console.log('Error',err);
-			return;
-		}
-		const s3params =
-		{
-			Bucket:				C.DIAGRAM_BUCKET_NAME,
-			ContentType:		'image/png',
-			ContentEncoding:	'base64',
-			Key:				name + '.png',
-			Body:				new Buffer.from(event.png.replace(/^data:image\/octet-stream;base64,/,""), 'base64'),
-			ACL:				'public-read',
-		};
-		const Item =
-		{
-			basename:		diagram.basename,
-			name,
-//			subkey:			'D-' + name,
-			timestamp:		diagram.timestamp,
-			user,
-//			username:		user,
-			description:	'description' in diagram ? diagram.description : '',
-			properName:		'properName' in diagram ? diagram.properName : diagram.basename,
-			references:		diagram.references,
-		};
-		const params =
-		{
-			TableName:  C.DIAGRAM_TABLE,
-			Item,
-		};
-		bucket.putObject(s3params, function(err, data)
-		{
-			if (err)
+//			const Body = JSON.stringify(diagram);
+//			const Body = buffer.toString('base64');
+			const Body = buffer;
+			if (Body.length > 1000000)
+				return callback('Error:  Diagram is too large to load');
+			if (diagram.user !== user)
+				return callback(`Error:  User ${user} is not the owner of ${diagram.name}.  ${diagram.user} is.`);
+			const bucket = new AWS.S3({apiVersion:'2006-03-01', params: {Bucket: C.DIAGRAM_BUCKET_NAME}});
+			const name = `${user}/${diagram.basename}`;
+			const Key = `${name}.json.gz`;
+			const s3params =
 			{
-				console.log('Error',err);
-				return;
-			}
-			AWS.config.update({region:C.REGION});
-			const db = new AWS.DynamoDB.DocumentClient();
-			db.put(params, function(err, data)
+				Bucket:  		C.DIAGRAM_BUCKET_NAME,
+				ContentType:	'json.gz',
+				Key,
+				Body,
+				ACL:	 		'public-read',
+			};
+			// save diagram.json
+			bucket.putObject(s3params, (err, data) =>
 			{
 				if (err)
+					return callback(err);
+				const s3params =
 				{
-					console.log("Error", err, data);
-					return;
-				}
-				AWS.config.update({region:C.REGION, credentials});
-				const msg =
-				{
-					Message:	JSON.stringify({user, name, timestamp:diagram.timestamp, cat:diagram.codomain}),
-					TopicArn:	C.CATECON_TOPIC
+					Bucket:				C.DIAGRAM_BUCKET_NAME,
+					ContentType:		'image/png',
+					ContentEncoding:	'base64',
+					Key:				name + '.png',
+					Body:				new Buffer.from(png.replace(/^data:image\/octet-stream;base64,/,""), 'base64'),
+					ACL:				'public-read',
 				};
-				const publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(msg).promise();
-				publishTextPromise.then(  function(data)
+				// save diagram png
+				bucket.putObject(s3params, (err, data) =>
 				{
-					console.log(`Message ${msg.Message} sent to the topic ${msg.TopicArn}`);
-					console.log("MessageID is " + data.MessageId);
-					delete Item.subkey;
+					if (err)
+						return callback(err);
+					AWS.config.update({region:C.REGION});
+					const db = new AWS.DynamoDB.DocumentClient();
+					const Item =
+					{
+						basename:		diagram.basename,
+						name,
+						timestamp,
+						user,
+						description:	'description' in diagram ? diagram.description : '',
+						properName:		'properName' in diagram ? diagram.properName : diagram.basename,
+						references:		diagram.references,
+					};
 					const params =
 					{
-						TableName:  C.RECENT_DIAGRAM_TABLE,
+						TableName:  C.DIAGRAM_TABLE,
 						Item,
 					};
-					db.put(params, function(err, data)
+					// save diagram info to db
+					db.put(params, (err, data) =>
 					{
 						if (err)
+							return callback(err);
+						const params =
 						{
-							console.log("Error", err, data);
-							return;
-						}
-						console.log(C.RECENT_DIAGRAM_TABLE, 'putItem succeeded', err, data);
-						callback(err, data);
+							TableName:  C.RECENT_DIAGRAM_TABLE,
+							Item,
+						};
+						// save to recent diagram table
+						db.put(params, function(err, data)
+						{
+							if (err)
+								return callback(err);
+							console.log(C.RECENT_DIAGRAM_TABLE, 'putItem succeeded', diagram.name);
+							callback(null, {status:"Success"});
+						});
 					});
-				}).catch(function(err)
-				{
-					console.error(err, err.stack);
 				});
 			});
 		});
