474c7f175f92ce36c63acdaa428b14fb0731cd0e
Harry Dole <harry@harrydole.com>
2021-03-18 20:47:35 -0700

    updated sql for diagrams

--- sql/Catecon.sql
+++ sql/Catecon.sql
@@ -4,19 +4,21 @@ CREATE TABLE `users` (`name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NO
 	`email` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
 	`permissions` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
 	`maxDiagrams` bigint(20) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-CREATE TABLE `diagrams` (`name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-	`basename` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-	`user` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-	`category` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-	`description` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-	`properName` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-	`refs` longtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-	`timestamp` bigint(20) NOT NULL,
-	`refcnt` int DEFAULT 0) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-ALTER TABLE `diagrams`
-	ADD UNIQUE KEY `Name` (`name`) USING HASH,
-	ADD KEY `TimestampIndex` (`timestamp`),
-	ADD KEY `user` (`user`(64));
-ALTER TABLE `diagrams` ADD FULLTEXT KEY `DescriptionIndex` (`properName`);
-CREATE USER `catecon`@`172.26.11.254` IDENTIFIED BY `ILoveCategoryTheory`;
-GRANT ALL PRIVILEGES ON `Catecon`.* TO `catecon`@`172.26.11.254`;
+ CREATE TABLE `diagrams` (
+  `name` varchar(128) DEFAULT NULL,
+  `basename` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `user` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `codomain` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `properName` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `refs` longtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `timestamp` bigint(20) NOT NULL,
+  `refcnt` int(11) DEFAULT '0',
+  `cloudTimestamp` bigint(20) DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  FULLTEXT KEY `DescriptionIndex` (`properName`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4
+CREATE USER `catecon`@`XXX.XXX.XXX.XXX` IDENTIFIED BY `ILoveCategoryTheory`;
+GRANT ALL PRIVILEGES ON `Catecon`.* TO `catecon`@`XXX.XXX.XXX.XXX`;
