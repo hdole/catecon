f1f0edba66b7abe833a22fc3d65acc0aa8f3b1e7
Harry Dole <harry@harrydole.com>
2021-12-28 13:08:59 -0800

    add intersection with a bezier curve

--- public/js/D2.js
+++ public/js/D2.js
@@ -307,6 +307,168 @@ class D2
 				D2.SegmentIntersect(pt0.x, pt0.y, pt1.x, pt1.y, box.x, box.y + box.height, box.x + box.width, box.y + box.height) ||	// bottom edge
 				D2.SegmentIntersect(pt0.x, pt0.y, pt1.x, pt1.y, box.x + box.width, box.y, box.x + box.width, box.y + box.height);		// right edge
 	}
+	// https://www.particleincell.com/2013/cubic-line-intersection/
+	static SortSpecial(a)
+	{
+		let flip;
+		let temp;
+		do
+		{
+			flip=false;
+			for (var i=0;i<a.length-1;i++)
+			{
+				if ((a[i+1]>=0 && a[i]>a[i+1]) ||
+					(a[i]<0 && a[i+1]>=0))
+				{
+					flip=true;
+					temp=a[i];
+					a[i]=a[i+1];
+					a[i+1]=temp;
+				}
+			}
+		}
+		while (flip);
+		return a;
+	}
+	static Sign(x)
+	{
+		return x < 0.0 ? -1 : 1;
+	}
+	// based on http://mysite.verizon.net/res148h4j/javascript/script_exact_cubic.html#the%20source%20code
+	static CubicRoots(P)
+	{
+		let t = [];
+		if(P[0] === 0)
+		{
+			if(P[1] === 0)		// linear
+			{
+				t[0] = -1 * ( P[3] / P[2] );
+				t[1] = -1;
+				t[2] = -1;
+				// discard out of spec roots
+				for (let i=0;i<1;i++)
+					if (t[i] > 1.0)
+						t[i] = -1;
+				// sort but place -1 at the end
+				t = D2.SortSpecial(t);
+				return t;
+			}
+			// quadratic
+			let DQ = Math.pow(P[2], 2) - 4*P[1]*P[3]; // quadratic discriminant
+			if( DQ >= 0 )
+			{
+				DQ = Math.sqrt(DQ);
+				t[0] = -1 * ( ( DQ + P[2] ) / ( 2 * P[1] ) );
+				t[1] = ( ( DQ - P[2] ) / ( 2 * P[1] ) );
+				t[2] = -1;
+				// discard out of spec roots
+				for (let i=0;i<2;i++)
+					if (t[i] > 1.0)
+						t[i]=-1;
+				// sort but place -1 at the end
+				t = D2.SortSpecial(t);
+				return t;
+			}
+		}
+		const a = P[0];
+		const b = P[1];
+		const c = P[2];
+		const d = P[3];
+		const A = b/a;
+		const B = c/a;
+		const C = d/a;
+		let Im;
+		const Q = (3*B - Math.pow(A, 2))/9;
+		const R = (9*A*B - 27*C - 2*Math.pow(A, 3))/54;
+		const D = Math.pow(Q, 3) + Math.pow(R, 2);    // polynomial discriminant
+		if (D >= 0)                                 // complex or duplicate roots
+		{
+			const S = D2.Sign(R + Math.sqrt(D))*Math.pow(Math.abs(R + Math.sqrt(D)),(1/3));
+			const T = D2.Sign(R - Math.sqrt(D))*Math.pow(Math.abs(R - Math.sqrt(D)),(1/3));
+	 
+			t[0] = -A/3 + (S + T);                    // real root
+			t[1] = -A/3 - (S + T)/2;                  // real part of complex root
+			t[2] = -A/3 - (S + T)/2;                  // real part of complex root
+			Im = Math.abs(Math.sqrt(3)*(S - T)/2);    // complex part of root pair   
+			// discard complex roots
+			if (Im != 0)
+			{
+				t[1] = -1;
+				t[2] = -1;
+			}
+		}
+		else                                          // distinct real roots
+		{
+			const th = Math.acos(R/Math.sqrt(-Math.pow(Q, 3)));
+			t[0] = 2*Math.sqrt(-Q)*Math.cos(th/3) - A/3;
+			t[1] = 2*Math.sqrt(-Q)*Math.cos((th + 2*Math.PI)/3) - A/3;
+			t[2] = 2*Math.sqrt(-Q)*Math.cos((th + 4*Math.PI)/3) - A/3;
+			Im = 0.0;
+		}
+		// discard out of spec roots
+		for (let i = 0;i<3;i++) 
+			if (t[i] < 0 || t[i] > 1.0 || isNaN(t[i]))
+				t[i] = -1;
+		// sort but place -1 at the end
+		t = D2.SortSpecial(t);
+		return t;
+	}
+	static BezierCoeffs(P0, P1, P2, P3)
+	{
+		const Z = [];
+		Z[0] = -P0 + 3*P1 + -3*P2 + P3;
+		Z[1] = 3*P0 - 6*P1 + 3*P2;
+		Z[2] = -3*P0 + 3*P1;
+		Z[3] = P0;
+		return Z;
+	}
+	// computes intersection between a cubic spline and a line segment
+	static HasBezierIntersection(start, end, cp1, cp2, lineStart, lineEnd)
+	{
+		let X = [];
+		const A = lineEnd.y-lineStart.y;	    //A=y2-y1
+		const B = lineStart.x-lineEnd.x;	    //B=x1-x2
+		const C = lineStart.x * (lineStart.y - lineEnd.y) + lineStart.y*(lineEnd.x-lineStart.x);	//C=x1*(y1-y2)+y1*(x2-x1)
+		const bx = D2.BezierCoeffs(start.x, cp1.x, cp2.x, end.x);
+		const by = D2.BezierCoeffs(start.y, cp1.y, cp2.y, end.y);
+		const P = [];
+		P[0] = A*bx[0]+B*by[0];		// t^3
+		P[1] = A*bx[1]+B*by[1];		// t^2
+		P[2] = A*bx[2]+B*by[2];		// t
+		P[3] = A*bx[3]+B*by[3] + C;	// 1
+		const r = D2.CubicRoots(P);
+		// verify the roots are in bounds of the linear segment
+		for (let i = 0; i<3;i++)
+		{
+			t = r[i];
+			if (t === -1)
+				return false;
+			X[0] = bx[0]*t*t*t+bx[1]*t*t+bx[2]*t+bx[3];
+			X[1] = by[0]*t*t*t+by[1]*t*t+by[2]*t+by[3];            
+			//above is intersection point assuming infinitely long line segment, make sure we are also in bounds of the line
+			let s;
+			if ((lineEnd.x-lineStart.x)!=0)           // if not vertical line
+				s = (X[0]-lineStart.x)/(lineEnd.x-lineStart.x);
+			else
+				s = (X[1]-lineStart.y)/(lineEnd.y-lineStart.y);
+			// in bounds?
+			if (!(t<0 || t>1.0 || s<0 || s>1.0))
+				return true;
+		}    
+		return false;
+	}
+	static BoxBezierIntersection(start, cp1, cp2, end, box)
+	{
+		if (D2.HasBezierIntersection(start, end, cp1, cp2, {x:box.x, y:box.y}, {x:box.x + box.width, y:box.y}))
+			return true;
+		if (D2.HasBezierIntersection(start, end, cp1, cp2, {x:box.x, y:box.y}, {x:box.x, y:box.y + box.height}))
+			return true;
+		if (D2.HasBezierIntersection(start, end, cp1, cp2, {x:box.x, y:box.y + box.height}, {x:box.x + box.width, y:box.y + box.height}))
+			return true;
+		if (D2.HasBezierIntersection(start, end, cp1, cp2, {x:box.x + box.width, y:box.y}, {x:box.x + box.width, y:box.y + box.height}))
+			return true;
+		return false;
+	}
 }
 
 if (typeof window === 'object')
