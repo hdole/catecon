4d481c8a116bdd94def8321fc46cd9cfc94093ad
Harry Dole <harry@harrydole.com>
2022-05-30 13:07:29 -0700

    add token based check for signup

--- lambda/CateconCheckSignup.js
+++ lambda/CateconCheckSignup.js
@@ -1,10 +1,11 @@
-// (C) 2018 Harry Dole
+// (C) 2018-2022 Harry Dole
 // Catecon:  The Categorical Console
 //
-// Return the most recent diagrams.
 //
+
 const AWS = require('aws-sdk');
 const C = require('./AWSconstants.js');
+const mysql = require('mysql')
 
 AWS.config.update(
 {
@@ -12,27 +13,67 @@ AWS.config.update(
 	credentials:	new AWS.CognitoIdentityCredentials({IdentityPoolId:C.IDENTITY_POOL_ID}),
 });
 
-exports.handler = (event, context, callback) =>
+const pool = mysql.createPool(
 {
-	const db = new AWS.DynamoDB({region:C.REGION});
-	const params =
+	host:		C.MYSQL_HOST,
+	user:		C.MYSQL_USER,
+	password:	C.MYSQL_PASSWORD,
+	database:	C.MYSQL_DATABASE,
+});
+
+exports.handler =  (e, context, callback) =>
+{
+	context.callbackWaitsForEmptyEventLoop = false;
+	e.response.autoConfirmUser = false;
+	const attrs = e.request.userAttributes;
+	const secret = attrs['custom:secret'];
+	const user = e.userName;
+	if (user.length < 5)
 	{
-		TableName:					C.DIAGRAM_TABLE,
-		Select:						'COUNT',
-	};
-	const e = event;
-	db.scan(params, function(err, data)
+		callback(new Error('User name must be five or more characters'), e);
+		return;
+	}
+	pool.getConnection((err, conn) =>
 	{
-		if (err)
+		conn.query(`SELECT * FROM tokens WHERE secret='${secret}';`, (err, results, fields) =>
 		{
-			console.log("Error", err, data);
-			return;
-		}
-		console.log('count', data, e);
-
-		e.response.autoConfirmUser = data.Count <= C.USERCOUNT;
-
-		console.log('autoConfirmUser', e.response.autoConfirmUser);
-		callback(null, e);
+			if (err)
+			{
+				callback(err);
+				return;
+			}
+			if (results.length === 0)
+			{
+				conn.release();
+				console.log('secret not found', secret);
+				callback(new Error('Your categorical secret is not good enough'), e);
+				return;
+			}
+			if (results[0].user === null && results[0].consumed === null)
+			{
+				const sql = `UPDATE tokens set user='${user}',consumed='${Date.now()}' WHERE secret='${secret}';`;
+				conn.query(sql, (err, results, fields) =>
+				{
+					if (err)
+					{
+						conn.release();
+						console.log('error updating database', err, user, secret, sql);
+						callback(new Error('Cannot update database for new user'), e);
+						return;
+					}
+					conn.release();
+					callback(null, e);
+				});
+			}
+			else
+			{
+				conn.release();
+				console.log('secret already used', secret);
+				callback(new Error('Your categorical secret is already used'), e);
+				return;
+			}
+		});
 	});
 };
+
+
